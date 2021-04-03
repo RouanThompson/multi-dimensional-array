85e5f7f825043769418f165d44954dd6d4d2c449
rouanthompson <rouan.thompson@gmail.com>
2021-04-03 16:54:48 -0400

    Used a 2D array and printed  it

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -1,8 +1,18 @@
 package com.FirstJavaSteps;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // int [][] numbers = new int[2][3];
+	    // numbers[0][0] = 1;
+        // System.out.println(Arrays.deepToString(numbers));
+        // int [row][col] numbers
+        // deepToString is needed to print multi dimensional array
+
+        int [][] numbers = { { 1, 2 ,3}, {4, 5, 6} };
+        // this is a faster way to initialize a multi dimensional array
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
